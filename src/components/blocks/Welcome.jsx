623db0e731a54363c98d09a62149ab32f45e24c6
Jonathan Franco <jonathan.franco@epitech.eu>
2024-02-05 15:32:54 +0100

    Add: Added images to the homepage

--- src/components/blocks/Welcome.jsx
+++ src/components/blocks/Welcome.jsx
@@ -1,14 +1,14 @@
 import '../../../src/assets/css/components/blocks/welcome.css'
+import red from '../../../src/assets/images/blocks/red.jpg'
+import blue from '../../../src/assets/images/blocks/blue.jpg'
+
 
 function Welcome() {
 
     return (
         <div class='welcome-block'>
-            <h2 class='welcome-title'>Welcome to my React App</h2>
-            <p class='welcome-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum quidem ducimus enim exercitationem minus, porro neque explicabo officiis consectetur, doloribus iste ipsam placeat delectus suscipit laboriosam omnis voluptatibus. Veniam, eaque.</p>
-            <p class='welcome-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum quidem ducimus enim exercitationem minus, porro neque explicabo officiis consectetur, doloribus iste ipsam placeat delectus suscipit laboriosam omnis voluptatibus. Veniam, eaque.</p>
-            <p class='welcome-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum quidem ducimus enim exercitationem minus, porro neque explicabo officiis consectetur, doloribus iste ipsam placeat delectus suscipit laboriosam omnis voluptatibus. Veniam, eaque.</p>
-            <p class='welcome-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum quidem ducimus enim exercitationem minus, porro neque explicabo officiis consectetur, doloribus iste ipsam placeat delectus suscipit laboriosam omnis voluptatibus. Veniam, eaque.</p>
+            <img className='carrousel-image' src={red} alt='Fjord mountains' />
+            <img className='carrousel-image' src={blue} alt='Fjord mountains' />
         </div>
     )
 }
