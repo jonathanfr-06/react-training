44906c074853d0bcb1ea5d9393d57ddf0bea3ac7
Jonathan Franco <jonathan.franco@epitech.eu>
2024-02-05 15:43:27 +0100

    Add: Added Caroussel lib to the project

--- src/components/blocks/Welcome.jsx
+++ src/components/blocks/Welcome.jsx
@@ -1,14 +1,17 @@
 import '../../../src/assets/css/components/blocks/welcome.css'
-import red from '../../../src/assets/images/blocks/red.jpg'
+import red from '../../../src/assets/images/blocks/red.jpeg'
 import blue from '../../../src/assets/images/blocks/blue.jpg'
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 function Welcome() {
 
     return (
         <div class='welcome-block'>
+            <Carousel autoPlay={true} infiniteLoop={true} showArrows={false} showStatus={false} interval={5000}>
             <img className='carrousel-image' src={red} alt='Fjord mountains' />
             <img className='carrousel-image' src={blue} alt='Fjord mountains' />
+            </Carousel>
         </div>
     )
 }
