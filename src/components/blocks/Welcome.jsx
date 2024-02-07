50495fda4e2db099b2d07eec32a6c4e5582250d0
Jonathan Franco <jonathan.franco@epitech.eu>
2024-02-07 21:36:19 +0100

    Add: Added better pictures to have a better quality in the slider.

--- src/components/blocks/Welcome.jsx
+++ src/components/blocks/Welcome.jsx
@@ -1,6 +1,11 @@
 import '../../../src/assets/css/components/blocks/welcome.css'
 import red from '../../../src/assets/images/blocks/red.jpg'
 import blue from '../../../src/assets/images/blocks/blue.jpg'
+import night from '../../../src/assets/images/blocks/night.jpeg'
+import green from '../../../src/assets/images/blocks/green.jpeg'
+
+
+
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
@@ -8,9 +13,14 @@ function Welcome() {
 
     return (
         <div class='welcome-block'>
-            <Carousel className='carousel-block' max-height="450px" dynamicHeight={true} autoPlay={true} infiniteLoop={true} showArrows={false} showIndicators={true} showStatus={false} interval={5000}>
+            <Carousel className='carousel-block' autoPlay={true} infiniteLoop={true} showArrows={false} showIndicators={true} showStatus={false} interval={5000}>
             <img className='carrousel-image' src={red} alt='Sunset' />
             <img className='carrousel-image' src={blue} alt='Fjord mountains' />
+            <img className='carrousel-image' src={green} alt='Green landscape mountains' />
+            <img className='carrousel-image' src={night} alt='Night mountains' />
+
+
+
             </Carousel>
         </div>
     )
