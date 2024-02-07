0599cd82b34baadc41d8e21b44baf1c0ff23a833
Jonathan Franco <jonathan.franco@epitech.eu>
2024-02-07 21:38:46 +0100

    Add: Modifier the welcome.jsx to slider.jsx along the variables names

--- src/components/blocks/Slider.jsx
+++ src/components/blocks/Slider.jsx
@@ -9,10 +9,10 @@ import green from '../../../src/assets/images/blocks/green.jpeg'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
-function Welcome() {
+function Slider() {
 
     return (
-        <div class='welcome-block'>
+        <div class='slider-block'>
             <Carousel className='carousel-block' autoPlay={true} infiniteLoop={true} showArrows={false} showIndicators={true} showStatus={false} interval={5000}>
             <img className='carrousel-image' src={red} alt='Sunset' />
             <img className='carrousel-image' src={blue} alt='Fjord mountains' />
@@ -26,4 +26,4 @@ function Welcome() {
     )
 }
 
-export default Welcome;
+export default Slider;
