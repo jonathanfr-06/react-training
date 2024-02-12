bd9e31df34e8ea066c3e4738c8fc6af246208ca7
Jonathan Franco <jonathan.franco@epitech.eu>
2024-02-12 10:31:15 +0100

    Add: Random image in the product list

--- src/components/products/List.jsx
+++ src/components/products/List.jsx
@@ -1,13 +1,29 @@
 import '../../../src/assets/css/components/products/list.css'
 import products  from '../../utils/products';
-import logo from '../../../src/assets/images/products/picture.png'
 import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import {  getRandomListPictures} from '../../services/lorempicsum';
 
 function List() {
-    const listItems = products.map((product) => (
+    const [imagesProduct, setImagesProduct] = useState([]);
+
+    async function getImgs() {
+        const imagess= await getRandomListPictures(products.length);
+        for(let i = 0; i < imagess.length; i++ ){
+         setImagesProduct(prevImages => [...prevImages, imagess[i].download_url]);
+         console.log(imagess[i]);
+        }
+     }
+
+    useEffect(() => {
+        setImagesProduct([]);
+        getImgs();
+    }, []);
+
+    const listItems = products.map((product, i) => (
         <div class='product-list-card'>
             <h1 className='product-list-name'>{product.name.toUpperCase()}</h1>
-            <img className='product-list-img' src={logo} alt={product.name} />
+            <img className='product-list-img' src={imagesProduct[i]} alt={product.name} />
             <p className="product-list-category">Catégorie : {product.category}</p>
             <p className='product-list-desc'>{product.desc} </p>
             <p className='product-list-price'>Prix : {product.price} € </p>
